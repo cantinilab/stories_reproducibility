e4e921a67ae5c0b4291575075960b774300c80be
Geert-Jan Huizing <30904288+gjhuizing@users.noreply.github.com>
2022-12-12 10:41:45 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# taken from https://github.com/CEA-COSMIC/ModOpt/blob/master/setup.py
+with open('requirements.txt') as open_file:
+    install_requires = open_file.read()
+
+setuptools.setup(
+    name="my-python-package-template",
+    version="0.0.1",
+    author="Zaccharie Ramzi",
+    author_email="zaccharie.ramzi@gmail.com",
+    description="My Python package template.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CSDUlm/my-python-package-template",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    extras_require={
+        'test': ['pytest'],
+    },
+    python_requires='>=3.6',
+)
