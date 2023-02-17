213c0019afab464d7858cc9c6cb7ae9e9754e4e0
Geert-Jan Huizing <huizing@ens.fr>
2023-02-17 14:20:38 +0100

    init with poetry

--- README.md
+++
@@ -1,3 +0,0 @@
-# Multi-omics trough space and time
-
-![GitHub Workflow Build Status](https://github.com/gjhuizing/space-time/workflows/Continuous%20testing/badge.svg)
--- build_package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-rm -f -r build/*
-rm -f -r dist/*
-
-pip install --upgrade setuptools wheel twine
-
-python setup.py sdist bdist_wheel
-twine upload -r testpypi dist/*
