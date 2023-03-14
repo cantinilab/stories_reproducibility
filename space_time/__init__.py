78298dc17ea2de638d9ff8d13c6b9f3f4f331ec2
Geert-Jan Huizing <huizing@ens.fr>
2023-03-14 18:30:06 +0100

    add a tools module

--- space_time/__init__.py
+++ space_time/__init__.py
@@ -5,5 +5,6 @@ import space_time.implicit_steps
 import space_time.model
 import space_time.potentials
 import space_time.simulate
+import space_time.tools
 from space_time.explicit_steps import linear, quadratic
-from space_time.implicit_steps import linear, monge_linear, quadratic
+from space_time.implicit_steps import linear, monge_linear, monge_quadratic, quadratic
