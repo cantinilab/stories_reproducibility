a745e9c3b7c7d9d4419f84c6913930348b2d0a58
Geert-Jan Huizing <huizing@ens.fr>
2023-03-17 15:04:38 +0100

    interpolate on high dimensional data

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from flatten_dict import flatten
 from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(version_base=None, config_path="configs", config_name="celegans")
+@hydra.main(version_base=None, config_path="configs/main", config_name="config")
 def main(cfg: DictConfig) -> None:
 
     import anndata as ad
+    import flax.linen as nn
     import jax
     import jax.numpy as jnp
     import optax
+    import scanpy as sc
+    from scipy import interpolate
     from space_time import explicit_steps, implicit_steps, model, potentials
 
     # start a new wandb run to track this script
@@ -27,11 +30,16 @@ def main(cfg: DictConfig) -> None:
 
     # Load the data.
     print("Loading data")
-    adata = ad.read_h5ad(cfg.dataset.path)
+    adata = ad.read_h5ad(cfg.organism.dataset_path)
+
+    sc.pp.subsample(adata, fraction=cfg.organism.subset)
 
     # Define the protential.
     print("Defining potential")
-    potential = potentials.MLPPotential(features=cfg.potential.features)
+    potential = potentials.MLPPotential(
+        features=cfg.potential.features,
+        act_fn=nn.softplus,
+    )
 
     # Define the proximal step.
     print("Defining proximal step")
@@ -83,8 +91,8 @@ def main(cfg: DictConfig) -> None:
     my_model = model.SpaceTime(
         potential=potential,
         proximal_step=proximal_step,
-        tau=cfg.model.tau,
-        debias=cfg.model.debias,
+        tau=cfg.proximal_step.tau,
+        quadratic=cfg.model.quadratic,
         epsilon=cfg.model.epsilon,
         teacher_forcing=cfg.model.teacher_forcing,
         wb=True,
@@ -93,9 +101,9 @@ def main(cfg: DictConfig) -> None:
     # Fit the model.
     my_model.fit_adata(
         adata=adata,
-        time_obs=cfg.dataset.time_obs,
-        obsm=cfg.dataset.obsm,
-        space_obsm=cfg.dataset.space_obsm,
+        time_obs=cfg.organism.time_obs,
+        obsm=cfg.organism.obsm,
+        space_obsm=cfg.organism.space_obsm,
         optimizer=optax.adam(cfg.optimizer.learning_rate),
         n_iter=cfg.optimizer.n_iter,
         batch_iter=cfg.optimizer.batch_iter,
@@ -107,41 +115,58 @@ def main(cfg: DictConfig) -> None:
     potential_fn = lambda x: my_model.potential.apply(my_model.params, x)
 
     # Try inference.
-    max_time = max(adata.obs[cfg.dataset.time_obs])
-    idx = adata.obs[cfg.dataset.time_obs] == max_time
-    x = adata[idx].obsm[cfg.dataset.obsm]
-    space = adata[idx].obsm[cfg.dataset.space_obsm]
+    max_time = max(adata.obs[cfg.organism.time_obs])
+    idx = adata.obs[cfg.organism.time_obs] == max_time
+    x = adata[idx].obsm[cfg.organism.obsm]
+    space = adata[idx].obsm[cfg.organism.space_obsm]
     my_model.transform(x, space)
 
     # Make a 3d scatter plot of the data.
     fig = plt.figure(constrained_layout=True)
     ax = fig.add_subplot(111, projection="3d", elev=30, azim=40)
-    zz = potential_fn(jnp.array(adata.obsm[cfg.dataset.obsm]))
-    xx = adata.obsm[cfg.dataset.obsm][:, 0]
-    yy = adata.obsm[cfg.dataset.obsm][:, 1]
+    zz = potential_fn(jnp.array(adata.obsm[cfg.organism.obsm]))
+    xx = adata.obsm[cfg.organism.viz_obsm][:, 0]
+    yy = adata.obsm[cfg.organism.viz_obsm][:, 1]
     ax.scatter(
         xs=xx,
         ys=yy,
         zs=zz,
         s=cfg.plot.scatter.size,
-        c=adata.obs[cfg.dataset.time_obs],
+        c=adata.obs[cfg.organism.time_obs],
         cmap=cfg.plot.scatter.cmap,
         zorder=1,
         alpha=cfg.plot.scatter.alpha,
     )
 
-    # Plot the potential function as a surface.
-    xx, yy = jnp.linspace(min(xx), max(xx), 50), jnp.linspace(min(yy), max(yy), 50)
-    xx, yy = jnp.meshgrid(xx, xx, indexing="xy")
-    zz = potential_fn(jnp.stack([xx, yy], axis=-1))
-    ax.plot_surface(xx, yy, zz, alpha=0.5, zorder=-1)
-    ax.contour(xx, yy, zz, zdir="z", offset=zz.min(), **cfg.plot.contour)
+    # Interpolate the potential function.
+    xx_surface = jnp.linspace(min(xx), max(xx), 50)
+    yy_surface = jnp.linspace(min(yy), max(yy), 50)
+    xx_surface, yy_surface = jnp.meshgrid(xx_surface, yy_surface, indexing="xy")
+    interp = interpolate.RBFInterpolator(
+        jnp.vstack((xx.ravel(), yy.ravel())).T, zz, smoothing=1e-4
+    )
+    zz_surface = interp(jnp.vstack((xx_surface.ravel(), yy_surface.ravel())).T)
+    zz_surface = zz_surface.reshape(xx_surface.shape)
 
-    # Decorate the plot.
-    ax.set_xlabel(f"{cfg.dataset.obsm} 1")
-    ax.set_ylabel(f"{cfg.dataset.obsm} 2")
-    ax.set_zlabel("Potential")
+    # Plot the potential function as a surface.
+    ax.plot_surface(xx_surface, yy_surface, zz_surface, alpha=0.5, zorder=-1)
+    ax.contour(
+        xx_surface,
+        yy_surface,
+        zz_surface,
+        zdir="z",
+        offset=min(zz_surface.min(), zz.min()),
+        **cfg.plot.contour,
+    )
+
+    # # Decorate the plot.
+    ax.set_xlabel(f"{cfg.organism.viz_obsm} 1")
+    ax.set_ylabel(f"{cfg.organism.viz_obsm} 2")
+    ax.set_zlabel("Interpolated potential")
+
     plt.title(cfg.plot.title)
+    image = wandb.Image(plt)
+    wandb.log({"3d_potential": image})
     plt.show()
 
 
