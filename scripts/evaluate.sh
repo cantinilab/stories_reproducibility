a63460db1704f1f090c3a6d444b80267c773bd1a
Geert-Jan Huizing <huizing@ens.fr>
2024-04-11 10:29:44 +0200

    Delete useless .sh

--- scripts/evaluate.sh
+++
@@ -1,7 +0,0 @@
-poetry run python3 scripts/evaluate.py --multirun hydra/launcher=gpu_joker wandb.mode="offline"     rank=-1     checkpoint_path=$(ls -d /pasteur/appa/homes/ghuizing/space-time/tmp/* | grep zesta | tr '\n' ',' | sed 's/,$/\n/')
-poetry run python3 scripts/evaluate.py --multirun hydra/launcher=gpu_joker wandb.mode="offline"     rank=-1     checkpoint_path=$(ls -d /pasteur/appa/homes/ghuizing/space-time/tmp/* | grep artista | tr '\n' ',' | sed 's/,$/\n/')
-poetry run python3 scripts/evaluate.py --multirun hydra/launcher=gpu_big wandb.mode="offline"       rank=500     checkpoint_path=$(ls -d /pasteur/appa/homes/ghuizing/space-time/tmp/* | grep mosta | tr '\n' ',' | sed 's/,$/\n/')
-
-poetry run python3 scripts/evaluate_multi.py --multirun hydra/launcher=gpu_joker wandb.mode="offline"     checkpoint_path=$(ls -d /pasteur/appa/homes/ghuizing/space-time/tmp/* | grep zesta | tr '\n' ',' | sed 's/,$/\n/')
-poetry run python3 scripts/evaluate_multi.py --multirun hydra/launcher=gpu_joker wandb.mode="offline"     checkpoint_path=$(ls -d /pasteur/appa/homes/ghuizing/space-time/tmp/* | grep artista | tr '\n' ',' | sed 's/,$/\n/')
-poetry run python3 scripts/evaluate_multi.py --multirun hydra/launcher=gpu_largemem wandb.mode="offline"       checkpoint_path=$(ls -d /pasteur/appa/homes/ghuizing/space-time/tmp/* | grep mosta | tr '\n' ',' | sed 's/,$/\n/')
